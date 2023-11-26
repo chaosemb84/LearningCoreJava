3bdb29a27078fdcd27785a11a88d9e7aa8661e14
chaosemb84 <ratish.jayemb@gmail.com>
2023-11-26 17:45:06 +0530

    day1-FirstJavaProgram

---
+++ src/main/java/com/techvantage/corejava/beginner/FirstJavaProgram.java
@@ -0,0 +1,7 @@
+package com.techvantage.corejava.beginner;
+
+public class FirstJavaProgram {
+    public static void main(String[] args) {
+        System.out.println("Learning to Code using Core Java");
+    }
+}
